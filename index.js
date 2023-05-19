ad7ee4605a565486c6cdc6f5e1a42b12eec35a30
seulgi <guseul9405@gmail.com>
2023-05-19 18:15:35 +0900

    서버 구축 중

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-// express 모듈 불러오기
+// 모듈 불러오기
 const express = require('express');
+const cors = require('cors');
 
 // express 객체 생성
 const app = express();
+
+const corsOptions = {
+    origin: 'http://211.237.50.150:7080', // 농림축산수산~
+};
+
+app.use(cors(corsOptions));
 
 // 기본 포트를 app 객체에 설정
 const port = 5001;
 app.listen(port);
 
 // 미들웨어 함수를 특정 경로에 등록
-app.use('/api/data', function(req, res) {
+app.use('/', (req, res) => {
+    res.redirect('https://todaydishes.netlify.app');
+})
+app.use('/api/data', (req, res) => {
     res.json({ greeting: 'Hello World' });
 });
 
