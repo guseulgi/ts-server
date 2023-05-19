d10db5ad93fde6708fdbbce54b3d726f69a7bbf7
seulgi <guseul9405@gmail.com>
2023-05-20 00:11:59 +0900

    오류 해결 중

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -14,27 +14,31 @@ const getAllFoodInfo = async (req, res) => {
   const recipeUrl = `${REACT_APP_API_URL}/openapi/${REACT_APP_API_KEY}/json/${REACT_APP_RECEIPE_GRID_NUM}/1/470`;
 
   try {
-    const result = await axios.get(basicUrl, {
-      withCredentials : true,
-    });
-    console.log(result.data[REACT_APP_BASIC_GRID_NUM].row);
+    const getInfo = async () => {
+      const result = await axios.get(basicUrl, {
+        withCredentials : true,
+      });
+      console.log(result.data[REACT_APP_BASIC_GRID_NUM].row);
+  
+      const resultSec = await axios.get(ingredientUrl, {
+        withCredentials: true,
+      });
+      console.log(resultSec.data[REACT_APP_INGREDIENT_GRID_NUM].row);
+  
+      const resultThir = await axios.get(recipeUrl, {
+        withCredentials: true,
+      });
+      console.log(resultThir.data[REACT_APP_RECEIPE_GRID_NUM].row);
+  
+      return res.status(200).json({
+        basicInfo : result.data[REACT_APP_BASIC_GRID_NUM].row,
+        ingredientInfo : resultSec.data[REACT_APP_INGREDIENT_GRID_NUM].row,
+        recipeInfo : resultThir.data[REACT_APP_RECEIPE_GRID_NUM].row,
+      });
 
-    const resultSec = await axios.get(ingredientUrl, {
-      withCredentials: true,
-    });
-    console.log(resultSec.data[REACT_APP_INGREDIENT_GRID_NUM].row);
-
-    const resultThir = await axios.get(recipeUrl, {
-      withCredentials: true,
-    });
-    console.log(resultThir.data[REACT_APP_RECEIPE_GRID_NUM].row);
-
-    return res.status(200).json({
-      basicInfo : result.data[REACT_APP_BASIC_GRID_NUM].row,
-      ingredientInfo : resultSec.data[REACT_APP_INGREDIENT_GRID_NUM].row,
-      recipeInfo : resultThir.data[REACT_APP_RECEIPE_GRID_NUM].row,
-    });
-
+    }
+          
+    getInfo();
   } catch(err) {
     console.error(err);
     res.status(500).send(err.message);
